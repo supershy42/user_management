3e19d3f9dcc0970ff6205a15618b2a0ff41af233
AhnJoonSung <tjdtna01@naver.com>
2024-11-10 23:57:32 +0900

    🧪 test: 회원가입 테스트 코드 작성
    이메일 인증을 제외한 회원가입 프로세스 테스트
    데이터베이스에 저장되는지 테스트

--- myproject/user_management/tests.py
+++ myproject/user_management/tests.py
@@ -1,3 +1,23 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+class UserRegistrationTest(APITestCase):
+    def test_user_registration(self):
+
+        data = {
+            'nickname': 'testuser',
+            'email': 'testuser@example.com',
+            'password': 'Testpass123!'
+        }
+
+        url = reverse('register')
+        response = self.client.post(url, data)
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        
+        self.assertIn("Registration successful", response.data.get("message", ""))
+        
+        from .models import User
+        user_exists = User.objects.filter(email=data['email']).exists()
+        self.assertTrue(user_exists)
