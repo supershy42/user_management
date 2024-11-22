b9e8b89b5d6c59930f1162f04150d655a10fd0c0
AhnJoonSung <tjdtna01@naver.com>
2024-11-22 18:52:47 +0900

    :sparkles: feat: user_profile_view

--- config/urls.py
+++ config/urls.py
@@ -2,5 +2,5 @@ from django.urls import path, include
 from user_management.views import UserRegistrationView, EmailVerificationView, UserLoginView
 
 urlpatterns = [
-    path('user/', include('user_management.urls')),
+    path('api/user/', include('user_management.urls')),
 ]
