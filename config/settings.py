16b41b1d5222c979902ab491056c4d451bc20f68
sujin Park <chemd78@naver.com>
2024-12-15 17:56:35 +0900

    :bug: fix: DEFAULT_AUTHENTICATION_CLASSES 제거
    - JWT 인증은 커스텀하여 진행하므로 기본 인증 절차 제거

--- config/settings.py
+++ config/settings.py
@@ -87,9 +87,6 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
-    ),
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
 }
 
