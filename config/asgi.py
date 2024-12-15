33d0137a4a081b552ef265039edadc1c8e107cae
sujin Park <chemd78@naver.com>
2024-12-15 16:18:24 +0900

    :sparkles: feat: ASGI 설정 추가
    - HTTP 및 WebSocket 라우팅 구성

--- config/asgi.py
+++ config/asgi.py
@@ -1,6 +1,13 @@
-from channels.routing import ProtocolTypeRouter
 from django.core.asgi import get_asgi_application
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+import user_management.routing
 
 application = ProtocolTypeRouter({
-    "http": get_asgi_application()
+    "http": get_asgi_application(),
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            user_management.routing.websocket_urlpatterns
+        )
+    )
 })
